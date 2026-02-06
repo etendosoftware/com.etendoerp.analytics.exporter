4fc14e3a50707c47ba55107f53f360b610a45776
Román Magnoli <roman.magnoli@smfconsulting.es>
2026-02-06 13:39:44 -0300

    Feature ETP-3104: Fix version compatibility

--- src/com/etendoerp/analytics/exporter/service/AnalyticsSyncService.java
+++ src/com/etendoerp/analytics/exporter/service/AnalyticsSyncService.java
@@ -4,7 +4,7 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.criterion.Order;
--- src/com/etendoerp/analytics/exporter/service/ReceiverHttpClient.java
+++ src/com/etendoerp/analytics/exporter/service/ReceiverHttpClient.java
@@ -9,7 +9,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
